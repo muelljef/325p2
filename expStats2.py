130d30946548cc4c67a08d13c963943b852d97aa
Jeff Mueller <muelljef@onid.oregonstate.edu>
2015-10-25 13:55:17 +1000

    Re-ran with project folder outside of cloud storage, cloud storage appeared to affect timings

--- expStats2.py
+++ expStats2.py
@@ -16,6 +16,8 @@ import csv
 #----------------------------------------------------------------
 # UTILITY FUNCTIONS
 #----------------------------------------------------------------
+
+
 def generate_list(start, end, interval):
     """ returns a list of integers separated by interval from start to end inclusive"""
     arr = []
@@ -27,36 +29,29 @@ def generate_list(start, end, interval):
     print arr
     return arr
 
-def write_to_csv(file_name, x, y, time, alg_name):
+
+def write_to_csv(file_name, x, y, time, alg_name, bool_append=0):
+    amounts = "Amounts, "
     title = alg_name + ", "
-    with open(file_name, 'wb') as csvfile:
+    if bool_append:
+        write_mode = 'ab'
+    else:
+        write_mode = 'wb'
+    with open(file_name, write_mode) as csvfile:
         statswriter = csv.writer(csvfile)
-        csvfile.write(title)
+        csvfile.write(amounts)
         statswriter.writerow(x)
         csvfile.write(title)
         statswriter.writerow(y)
         csvfile.write(title)
         statswriter.writerow(time)
-    print " >>>>> done! Results written to", file_name
+    print " >>>>> done! ", alg_name, " Results written to ", file_name
 
-
-def append_to_csv(file_name, x, y, time, alg_name):
-    title = alg_name + ", "
-    with open(file_name, 'ab') as csvfile:
-        statswriter = csv.writer(csvfile)
-        csvfile.write(title)
-        statswriter.writerow(x)
-        csvfile.write(title)
-        statswriter.writerow(y)
-        csvfile.write(title)
-        statswriter.writerow(time)
-    print " >>>>> done! Results written to", file_name
 
 def generate_statistics(Amt, coins, file_name, alg_name, algorithm, bool_slow=0, bool_append=0):
     min_coins = []
     alg_time = []
     for a in Amt:
-        #print "\n"+str(alg_name)
         total_time = 0
         start = time.clock()
         if bool_slow:
@@ -67,46 +62,42 @@ def generate_statistics(Amt, coins, file_name, alg_name, algorithm, bool_slow=0,
         elapsed = time.clock() - start
         total_time += elapsed
         alg_time.append(total_time/10.0)
-    if bool_append:
-        append_to_csv(file_name, Amt, min_coins, alg_time, alg_name)
-    else:
-        write_to_csv(file_name, Amt, min_coins, alg_time, alg_name)
+    write_to_csv(file_name, Amt, min_coins, alg_time, alg_name, bool_append)
 
-"""
 ########## QUESTION 4 ##########
-V=[1,5,10,25,50]
+V=[1, 5, 10, 25, 50]
 A = generate_list(5, 250, 5)
-generate_statistics(A, V, '2 - Q4Greedy.csv', 'Greedy-Question4', changeAlgorithms.changegreedy)
-generate_statistics(A, V, '2 - Q4DP.csv', 'DP-Question4', changeAlgorithms.changedp)
-generate_statistics(A, V, '2 - Q4Slow.csv', 'Slow-Question4', changeAlgorithms.changeslow, 1)
-"""
+generate_statistics(A, V, 'Q4withslow.csv', 'Greedy-Question4', changeAlgorithms.changegreedy)
+generate_statistics(A, V, 'Q4withslow.csv', 'DP-Question4', changeAlgorithms.changedp, 0, 1)
+generate_statistics(A, V, 'Q4withslow.csv', 'Slow-Question4', changeAlgorithms.changeslow, 1, 1)
 
 ########## QUESTION 5 ##########
-V1 = [1,2,6,12,24,48,60]
-V2 = [1,6,13,37,150]
-A1 = generate_list(2,100,2)
+V1 = [1, 2, 6, 12, 24, 48, 60]
+V2 = [1, 6, 13, 37, 150]
+A1 = generate_list(2, 200, 2)
 
-def print_result_for_comparison(coins,A,greedy,dp):
+
+def print_result_for_comparison(coins, A, greedy, dp):
     for a in A:
         print "\n--Greedy--"
-        greedy(coins,a)
+        greedy(coins, a)
         print "--DP--"
-        dp(coins,a)
+        dp(coins, a)
 
-generate_statistics(A1, V1, '2 - Q5.csv', 'Q5 - greedy - V1', changeAlgorithms.changegreedy)
-generate_statistics(A1, V2, '2 - Q5.csv', 'Q5 - greedy - v2', changeAlgorithms.changegreedy, 0, 1)
-generate_statistics(A1, V1, '2 - Q5.csv', 'Q5 - dp - v1', changeAlgorithms.changedp, 0, 1)
-generate_statistics(A1, V2, '2 - Q5.csv', 'Q5 - dp - v2', changeAlgorithms.changedp, 0, 1)
-generate_statistics(A1, V1, '2 - Q5.csv', 'Q5 - slow - v1', changeAlgorithms.changeslow, 1, 1)
-generate_statistics(A1, V2, '2 - Q5.csv', 'Q5 - slow - v2', changeAlgorithms.changeslow, 1, 1)
+generate_statistics(A1, V1, 'Q5withSlow.csv', 'greedy-V1', changeAlgorithms.changegreedy)
+generate_statistics(A1, V1, 'Q5withSlow.csv', 'dp-v1', changeAlgorithms.changedp, 0, 1)
+generate_statistics(A1, V1, 'Q5withSlow.csv', 'slow-v1', changeAlgorithms.changeslow, 1, 1)
+
+generate_statistics(A1, V2, 'Q5withSlow.csv', 'greedy-v2', changeAlgorithms.changegreedy, 0, 1)
+generate_statistics(A1, V2, 'Q5withSlow.csv', 'dp-v2', changeAlgorithms.changedp, 0, 1)
+generate_statistics(A1, V2, 'Q5withSlow.csv', 'slow-v2', changeAlgorithms.changeslow, 1, 1)
 
 """
 ########## QUESTION 6 ##########
-V3=[1] + generate_list(2,30,2)
-#print V3
-generate_statistics(A1, V3, 'Q6Greedy.csv', 'Greedy-Question6', changeAlgorithms.changegreedy)
-generate_statistics(A1, V3, 'Q6DP.csv', 'DP-Question6', changeAlgorithms.changedp)
-
-
-########## QUESTION 7 ##########
+A3 = generate_list(2, 70, 2)
+V3 = [1] + generate_list(2, 30, 2)
+# print V3
+generate_statistics(A3, V3, 'Q6withSlow.csv', 'Greedy-Question6', changeAlgorithms.changegreedy)
+generate_statistics(A3, V3, 'Q6withSlow.csv', 'DP-Question6', changeAlgorithms.changedp, 0, 1)
+generate_statistics(A3, V3, 'Q6withSlow.csv', 'Slow-Question6', changeAlgorithms.changeslow, 1, 1)
 """
